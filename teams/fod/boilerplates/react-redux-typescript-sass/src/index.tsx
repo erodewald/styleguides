cc9fdb215247bc01f71e6e9bbdc08181e12c71b3
Eric Rodewald <eric.rode@gmail.com>
2017-07-11 10:22:23 -0500

    Removing boilerplate as we are moving them to their own repos

--- teams/fod/boilerplates/react-redux-typescript-sass/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import './index.scss';
-
-ReactDOM.render(
-    <div>Hello World</div>,
-    document.getElementById('root')
-);
